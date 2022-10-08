60e79c8bc7e121c061c3fafc9911fe343e276503
YildirimCat <yldrmcat68@hotmail.com>
2022-10-08 17:27:29 +0300

    App 1.0 is added.

--- homeGUI.py
+++ homeGUI.py
@@ -1,19 +1,3 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow
-
-class HomeGUI(QtWidgets.QStackedWidget):
-    def setup(self):
-        self.page_home = QtWidgets.QWidget()
-        self.page_home.setObjectName("page_home")
-        self.label_2 = QtWidgets.QLabel(self.page_home)
-        self.label_2.setGeometry(QtCore.QRect(190, 0, 131, 41))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_2.setFont(font)
-        self.label_2.setStyleSheet("color: rgb(81, 113, 140);")
-        self.label_2.setObjectName("label_2")
-    def retranslate(self,main):
-        _translate = QtCore.QCoreApplication.translate
-        self.label_2.setText(_translate("main", "Ana Ekran"))
+version https://git-lfs.github.com/spec/v1
+oid sha256:302ae759808bae4f7952c28a6e38975b2f5eaceada3f2a1ce45aec79befb495d
+size 785
--- update_profile.py
+++ update_profile.py
@@ -1,29 +1,3 @@
-from decimal import Decimal
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import pyqtSignal
-from guncelle_python import Ui_Guncelle
-
-class updateProfile(QMainWindow):
-    new_user_signal = pyqtSignal(list)
-
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Guncelle()
-        self.ui.setupUi(self)
-        self.ui.pushButton_guncelle.clicked.connect(self.update_profile)
-
-    def update_profile(self):
-        name = self.ui.lineEdit_guncelle_name.text()
-        age = self.ui.lineEdit_guncelle_age.text()
-        weight = self.ui.lineEdit_guncelle_weight.text()
-        boy = self.ui.lineEdit_guncelle_boy.text()
-        cinsiyet = self.ui.comboBox_guncelle_cinsiyet.currentText()
-        profile = [name, age, weight, boy, cinsiyet]
-
-        self.new_user_signal.emit(profile)
-    def clear(self):
-        self.ui.lineEdit_guncelle_name.clear()
-        self.ui.lineEdit_guncelle_age.clear()
-        self.ui.lineEdit_guncelle_weight.clear()
-        self.ui.lineEdit_guncelle_boy.clear()
-        self.ui.comboBox_guncelle_cinsiyet.clear()
+version https://git-lfs.github.com/spec/v1
+oid sha256:77a39d8b899c227bbb48134ab6680c1a47a02c840ad4fe394ac62c98b918b670
+size 1090
